567dd6da936b0c764f966420c650d5695348da9d
Olaf Lischke <olaf.lischke@lischke-edv.de>
2021-12-07 07:52:32 +0100

    Live Coding 1. Tag

---
+++ Live Coding/HalloWelt3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HalloWelt3
+{
+    class Program
+    {
+        // Haupteinstiegspunkt der Anwendung
+        static void Main(string[] args)
+        {
+            // Benutzer nach seinem Namen fragen
+            Console.WriteLine("Wie heißt Du?");
+            // Variablentyp Variablenname = Wert;
+            string benutzer = Console.ReadLine();
+
+            // string benutzer2;
+            // benutzer2=Console.ReadLine();
+
+            // Benutzer mit Name begrüßen
+            Console.WriteLine($"Hallo {benutzer}!"); // "Strings are immutable"
+        } 
+    }
+}
