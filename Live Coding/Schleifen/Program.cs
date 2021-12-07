e2359dda65769e571583ec4bf307603a5c082f0c
Olaf Lischke <olaf.lischke@lischke-edv.de>
2021-12-07 12:06:30 +0100

    Schleifen

---
+++ Live Coding/Schleifen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Schleifen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wieviele Durchläufe hättsn gern?");
+            int laeufe = Convert.ToInt32(Console.ReadLine()); //
+
+            string meldung;
+
+            for (int i = 0; i < laeufe; i++)
+            {
+                meldung = "For-Zähler: " + i;
+                Console.WriteLine(meldung);
+            }
+
+            // Geht nicht, weil i nicht mehr bekannt: Console.WriteLine($"Das waren Deine {i} Durchläufe.");
+
+            string zeichen="";
+            int zaehler=0;
+            do
+            {
+                zeichen = zeichen + "a";
+                Console.WriteLine("Do: " + zeichen);
+                zaehler++;
+            } while (zeichen.Length < 10 && zaehler < laeufe ); // Fußgesteuert
+
+           zeichen="";
+
+            while (zeichen.Length < 0) // Kopfgesteuert
+            {
+                zeichen = zeichen + "a";
+                Console.WriteLine("While: " + zeichen);
+            }
+
+        }
+    }
+}
