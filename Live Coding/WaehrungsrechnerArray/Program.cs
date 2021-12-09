c40986cce0bff12ce9bdac6ab7235270fadc0c08
Olaf Lischke <olaf.lischke@lischke-edv.de>
2021-12-09 09:53:44 +0100

    Upload Tag 3 neu

---
+++ Live Coding/WaehrungsrechnerArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Waehrungsrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrays bauen
+            string[] zeichen = new string[] { "USD", "JPY", "DKK", "SEK", "ZAR" };
+            double[] kurse = new double[] { 1.1256, 127.83, 7.4363, 10.2573, 17.9612 };
+
+            // Benutzer nach Währung fragen
+            Console.WriteLine("Welche Währung?");
+            string waehrung = Console.ReadLine().ToUpper();
+
+            // Benutzer nach Fremdbetrag fragen
+            Console.WriteLine($"Bitte gib einen Betrag in {waehrung} ein:");
+            double betrag = Convert.ToDouble(Console.ReadLine());
+
+            // In Euro umrechnen
+            int index = Array.IndexOf(zeichen, waehrung); // Wenn nix gefunden: -1
+            if (index >= 0)
+            {
+                double ergebnis = betrag / kurse[index];
+                // Ergebnis ausgeben
+                //Console.WriteLine("EUR: " + Math.Round(ergebnis, 4));
+                Console.WriteLine($"EUR: {ergebnis:0.00}");
+            }
+            else
+            {
+                Console.WriteLine("Unbekannte Währung!");
+            }
+        }
+    }
+}
---
+++ Live Coding/WaehrungsrechnerDictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WaehrungsrechnerDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dictionary (Wörterbuch, Telefonbuch) erzeugen
+            Dictionary<string, double> sorten = new Dictionary<string, double>();
+
+            // Dictionary mit Keys und Werten füllen
+            sorten.Add("USD", 1.1256);
+            sorten.Add("JPY", 127.83);
+            sorten.Add("ZAR", 17.9612);
+            sorten.Add("VND", 25892.90);
+
+            // Benutzer nach Währung fragen
+            Console.WriteLine("Welche Währung?");
+            string waehrung = Console.ReadLine().ToUpper().Trim();
+
+            // Benutzer nach Fremdbetrag fragen
+            Console.WriteLine($"Bitte gib einen Betrag in {waehrung} ein:");
+            double betrag = Convert.ToDouble(Console.ReadLine());
+
+            // In Euro umrechnen
+
+            // Gewünschte Währung im Dictionary vorhanden?
+            if (sorten.ContainsKey(waehrung)==true)
+            {
+                double ergebnis = betrag / sorten[waehrung]; // Dictionary-Value abfragen mit dictionary[key]
+                // Ergebnis ausgeben
+                //Console.WriteLine("EUR: " + Math.Round(ergebnis, 4));
+                Console.WriteLine($"EUR: {ergebnis:0.00}");
+            }
+            else
+            {
+                Console.WriteLine("Unbekannte Währung!");
+            }
+        }
+    }
+}
